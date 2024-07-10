cf7bb744e90eaf5d729244598206a2480c9232be
nara <me@nara.dev>
2024-07-10 22:38:37 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "narashin",
-    image: "./notion-avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "./naranara-svg.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Software Engineer",
     bio: "쓰고 만듭니다",
     email: "ca3rot@gmail.com",
